c5884c06fec94c679c69cddb41018253ddb36b91
gamza <rachu970605@gmail.com>
2025-12-12 16:52:17 +0900

    font 다운

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-const init = () => {
-    console.log("호출")
-    let baseURl = window.location.hostname //127.0.0.1 or localhost or ip or domain
-    let url = $(".logo a").attr("href"); // jquery attr attribute 속성 href = /subway/index.html
-    const hostname = !baseURl.includes("github") ? baseURl + ":5500" : baseURl + "/portfolio";
-    const fullUrl = "http://"+hostname + url;
-    $(".logo a").attr("href", fullUrl); //http://localhost:5500/subway/index.html ? http://githubdomain/protfolio/subway/index.html
-}
-
-
-
 $(function() {
 
   // gnb
